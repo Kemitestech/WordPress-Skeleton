4d47802c800cd3c23c93769b6c8a2d48cbf3ace8
Emmanuel <sinnell@aol.com>
2016-03-01 08:56:13 +0000

    added full-width template

---
+++ content/themes/elitepress-child/fullwidth.php
@@ -0,0 +1,18 @@
+<?php
+/**
+Template Name: Fullwidth
+*/
+get_header();
+?>
+<div class="content-section">
+	<div class="title-section">
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+			<?php get_template_part('content',''); ?>
+			</div>
+		</div>
+	</div>
+</div>
+<?php get_footer(); ?>
