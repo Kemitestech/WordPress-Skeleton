1d4d7a4ff1e42b93b78e4623457bf5831f0173fd
Emmanuel <sinnell@aol.com>
2016-03-02 17:08:58 +0000

    made latest events easier to click

--- content/themes/elitepress-child/index-about-us-section.php
+++ content/themes/elitepress-child/index-about-us-section.php
@@ -13,7 +13,7 @@
 					<div class = "row">
 						<div class = "col-md-6 col-sm-12">
 							<div id="about-text">
-								<p>The Arches Project has been founded with the sight <a href="<?php echo base_url();?>/artist-directory">to be a platform</a> for creative talents of all discipline.</p>
+								<p>The Arches Project has been founded with the sight <a href="<?php echo home_url();?>/artist-directory">to be a platform</a> for creative talents of all discipline.</p>
 								<p>
 									It gives emerging artists the opportunity to gain experience within the industry and achieve exposure they would not otherwise have received.
 We believe too many creative talents give up on their passion for what they have worked so hard to achieved as a result of the lack of ability to expose their work, with your donations we will be able to give more support to these artists whilst offering a better chance in life, providing opportunities to enable them to develop leadership skills, build self confidence, encourage and develop positive attitudes to pursue their aspirations.
