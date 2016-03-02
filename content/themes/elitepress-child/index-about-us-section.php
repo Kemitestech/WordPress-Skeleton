c8b3849581b0f6716468e760332d911da85ef328
Emmanuel <sinnell@aol.com>
2016-03-02 16:55:06 +0000

    updated about-us section

--- content/themes/elitepress-child/index-about-us-section.php
+++ content/themes/elitepress-child/index-about-us-section.php
@@ -13,21 +13,23 @@
 					<div class = "row">
 						<div class = "col-md-6 col-sm-12">
 							<div id="about-text">
-								<p>The Arches Project has been founded with the sight <a href="#">to be a platform</a> for creative talents of all discipline.</p>
+								<p>The Arches Project has been founded with the sight <a href="<?php echo base_url();?>/artist-directory">to be a platform</a> for creative talents of all discipline.</p>
 								<p>
 									It gives emerging artists the opportunity to gain experience within the industry and achieve exposure they would not otherwise have received.
 We believe too many creative talents give up on their passion for what they have worked so hard to achieved as a result of the lack of ability to expose their work, with your donations we will be able to give more support to these artists whilst offering a better chance in life, providing opportunities to enable them to develop leadership skills, build self confidence, encourage and develop positive attitudes to pursue their aspirations.
 								</p>
 								<p>This passion is too important to lose faith in.</p>
+								<p>We are offering to our artists and the community an art cafe which will give them the experience, exposure and confidence needed to enter into the professional realm.</p>
+								<p>Be bold......Be seen.......Be recognised.</p>
 							</div>
 						</div>
 						<div class ="col-md-4 col-sm-12 col-md-offset-1">
-							<?php echo do_shortcode('[custom-facebook-feed id=TheArchesProject num=1 type=photos include=likebox,text,date,eventtitle,author]'); ?>
+							<?php echo do_shortcode('[custom-facebook-feed id=TheArchesProject num=2 type=photos include=likebox,text,date,eventtitle,author]'); ?>
 						</div>
-					</div>		
+					</div>
 					</div>
 				</div>
-				
+
 			</div>
 		</div>
 	</div>
